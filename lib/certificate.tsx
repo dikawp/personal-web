20d2fa7033ceaa3e621383fbb6b774d18572283f
dikawp <ramadika380@gmail.com>
2025-10-02 19:06:52 +0700

    minor rev

--- lib/certificate.tsx
+++ lib/certificate.tsx
@@ -1,51 +1,93 @@
 export const certificates = [
   {
-    id: 6,
+    id: 12,
+    title: 'SQL Certification Test (Basic)',
+    publisher: 'Hackerrank',
+    image: '/certificate/hackerrank.webp',
+    link: 'https://www.hackerrank.com/certificates/34a4a627757d'
+  },
+  {
+    id: 11,
     title: 'English Proficiency Test (EPrT) ',
     publisher: 'Language Center Telkom University',
     image: '/certificate/eprt.webp',
     link: 'https://igracias.telkomuniversity.ac.id/LACValidation/index.php?id=2175926&id2=84&id3=2178264&id4=1204210024'
   },
   {
-    id: 5,
+    id: 10,
     title: 'Magang Bersertifikat -  MSIB Batch 6',
     publisher: 'Kampus Merdeka',
     image: '/certificate/msib.webp',
     link: 'https://drive.google.com/file/d/1pg7Pzil7q0FElHcvMXFuqU7KgdEXLqkm/view?usp=sharing'
   },
   {
-    id: 4,
+    id: 9,
+    title: 'Belajar Dasar Structured Query Language (SQL)',
+    publisher: 'Dicoding Academy',
+    image: '/certificate/dicoding_sql.webp',
+    link: 'https://www.dicoding.com/certificates/72ZDKYRWVPYW'
+  },
+  {
+    id: 8,
+    title: 'Belajar Dasar Data Science',
+    publisher: 'Dicoding Academy',
+    image: '/certificate/dicoding_ds.webp',
+    link: 'https://www.dicoding.com/certificates/1OP8J1QQLPQK'
+  },
+  {
+    id: 7,
+    title: 'Belajar Dasar AI',
+    publisher: 'Dicoding Academy',
+    image: '/certificate/dicoding_ai.webp',
+    link: 'https://www.dicoding.com/certificates/07Z6J8OMJXQR'
+  },
+  {
+    id: 6,
     title: 'Belajar Dasar Pemrograman JavaScript',
     publisher: 'Dicoding Academy',
     image: '/certificate/dicoding_js.webp',
     link: 'https://www.dicoding.com/certificates/QLZ9416E7P5D'
   },
   {
-    id: 3,
+    id: 5,
     title: 'Belajar Dasar Pemrograman Web',
     publisher: 'Dicoding Academy',
     image: '/certificate/dicoding_dasar.webp',
     link: 'https://www.dicoding.com/certificates/MRZMLY730XYQ'
   },
   {
-    id: 2,
+    id: 4,
     title: 'SQL Certification Course',
     publisher: 'Intellipaat Academy',
     image: '/certificate/intelli_sql.webp',
     link: 'https://intellipaat.com/academy/certificate-link/?Yz02MTkmdT0yMDc3NzkmZXh0PTE='
   },
   {
-    id: 1,
+    id: 3,
     title: 'Python Certification Course',
     publisher: 'Intellipaat Academy',
     image: '/certificate/intelli_sql.webp',
     link: 'https://intellipaat.com/academy/certificate-link/?Yz0xNTQzJnU9MjA3Nzc5JmV4dD0x'
   },
   {
-    id: 0,
+    id: 2,
     title: 'Artificial Intelligence (AI) Course',
     publisher: 'Intellipaat Academy',
     image: '/certificate/intelli_ai.webp',
     link: 'https://intellipaat.com/academy/certificate-link/?Yz0xODI3JnU9MjA3Nzc5JmV4dD0x'
+  },
+  {
+    id: 1,
+    title: 'Workshop WAOW Season 2 - Back End Development',
+    publisher: 'CODER - IT Telkom Surabaya',
+    image: '/certificate/waowb.webp',
+    link: 'https://drive.google.com/file/d/1rXEwttu3uisTNRVet8Fy-x6A8_wRSRRG/view?usp=sharing'
+  },
+  {
+    id: 0,
+    title: 'Workshop WAOW Season 2 - Front End Development',
+    publisher: 'CODER - IT Telkom Surabaya',
+    image: '/certificate/waowf.webp',
+    link: 'https://drive.google.com/file/d/1VymqCP6XI41Z2Mzv_kq6gTx4oxOgjJCt/view?usp=sharing'
   },
 ];
